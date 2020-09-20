0f24ebd39ee2ea772c9b6089916f075ed2f10c9c
李振 <lizhen@hujiang.com>
2020-09-20 12:17:11 +0800

    fix bug

--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,6 @@
+export {default as FunkyBar} from './components/FancyBar'
 // import FancyBar from './components/FancyBar.js';
-import React from "react";
+// import React from "react";
 
 
 // const FunkyBar:React.FC = (props) => <div><FancyBar /></div>;
