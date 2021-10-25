2831f6ebc7fe8c83ef7e3bda9eb23cfc145ee23b
李振 <lizhen@hujiang.com>
2021-10-25 22:11:44 +0800

    add postcss config

--- webpack.config.js
+++ webpack.config.js
@@ -26,7 +26,7 @@ module.exports = {
             },
             {
                 test: /\.s?css?$/,
-                use: ["style-loader", "css-loader", "sass-loader"],
+                use: ["style-loader", "css-loader", "postcss-loader", "sass-loader"],
                 exclude: /node_modules/,
             },
         ]
